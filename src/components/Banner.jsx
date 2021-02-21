5b7997feba774cd58e40ee93794e2dc5ce14372f
1Canis-Lupus1 <shubhamchoudhary081997@gmail.com>
2021-02-21 17:52:28 +0530

    Banner Updated

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,43 +1,58 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import axios from "axios";
+import "../Banner.css";
 
 function Banner(props) {
-    const [movie,setMovie]=useState([]);
+  const [movie, setMovie] = useState([]);
 
-    useEffect(()=>{
-        async function fetchData(){
-            const request=await axios.get(props.fetchUrl);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(props.fetchUrl);
 
-            // request.data.results[Math.floor(Math.random()*request.data.results.length - 1)] => Will Display any random movie from the movie list
-            setMovie(request.data.results[Math.floor(Math.random()*request.data.results.length - 1)]);
+      // request.data.results[Math.floor(Math.random()*request.data.results.length - 1)] => Will Display any random movie from the movie list
+      setMovie(
+        request.data.results[
+          Math.floor(Math.random() * request.data.results.length - 1)
+        ]
+      );
 
-            return request;
-        }
-        fetchData();
-    },[props.fetchUrl])
+      return request;
+    }
+    fetchData();
+  }, [props.fetchUrl]);
 
-    console.log(movie)
+  console.log(movie);
 
-    return (
-        <header className="banner" style={{
-            backgroundSize:"cover",
-            backgroundPosition:"center center",
-            backgroundImage:`url("http://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`
-        }} >{/*<==Background image */}
-        <div className="banner-content">
+//   For Truncating the description
+  function truncate(str, n) {
+    return str?.length > n ? str.substr(0, n - 1) + " ..." : str;
+  }
 
-            {/* {Title} */}
-            <h1>{movie?.title || movie?.name || movie?.original_name}</h1>
-            {/* {div=>2 buttons} */}
-            <div className="banner-btn">
-                <button className="btn">Play</button>
-                <button className="btn">My List</button>
-            </div>
-            {/* {description} */}
-            <h1 className="banner-desc">{movie.overview}</h1>
+  return (
+    <header
+      className="banner"
+      style={{
+        backgroundSize: "cover",
+        backgroundPosition: "center center",
+        backgroundImage: `url("http://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+      }}
+    >
+      {/*<==Background image */}
+      <div className="banner-content">
+        {/* {Title} */}
+        <h1 className="banner-title">
+          {movie?.title || movie?.name || movie?.original_name}
+        </h1>
+        {/* {div=>2 buttons} */}
+        <div className="banner-btn">
+          <button className="btn">Play</button>
+          <button className="btn">My List</button>
         </div>
-        </header>
-    )
+        {/* {description} */}
+        <h1 className="banner-desc">{truncate(movie?.overview, 150)}</h1>
+      </div>
+    </header>
+  );
 }
 
-export default Banner
+export default Banner;
